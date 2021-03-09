b8638a90204a4928abcfcb50ec2697258b387861
Dario Martinez <gusdamtzr@gmail.com>
2021-03-09 15:21:38 +0000

    Adds Cloud Datastore to save message

--- portfolio/src/main/java/com/google/sps/servlets/ContactMeServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ContactMeServlet.java
@@ -5,6 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.KeyFactory;
 
 @WebServlet("/contact-me")
 public class ContactMeServlet extends HttpServlet{
@@ -13,11 +18,19 @@ public class ContactMeServlet extends HttpServlet{
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String email = request.getParameter("email");
         String message = request.getParameter("message");
+        long timestamp = System.currentTimeMillis();
 
-        String log = "Email: " + email + "\nMessage: " + message;
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        KeyFactory keyFactory = datastore.newKeyFactory().setKind("Message");
+        FullEntity messageEntity = 
+            Entity.newBuilder(keyFactory.newKey())
+                .set("email", email)
+                .set("message", message)
+                .set("timestamp", timestamp)
+                .build();
 
-        System.out.println(log);
+        datastore.put(messageEntity);
+        System.out.printf("Email: %s\nMessage: %s\n", email, message);
         response.sendRedirect("/confirm.html");
-
     }
 }
